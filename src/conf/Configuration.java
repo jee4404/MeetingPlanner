fbd591ef7abbb4dd5e900ebd98ffd456c01113a4
Jee4404 <mour03@uqo.ca>
2014-11-13 21:09:03 -0500

    conf : dblocation et dblocationurl

--- src/conf/Configuration.java
+++ src/conf/Configuration.java
@@ -2,6 +2,9 @@ package conf;
 
 /**
  * Created by Rémy on 2014-11-13.
+ * Classe de configuration - seulement des attributs publics et statiques
  */
 public class Configuration {
+    public static final String dbLocation = "";
+    public static final String dbURL = "";
 }
