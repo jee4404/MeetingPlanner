71f050c95cbf0d0b2cffff0499630f1009c6ef0f
Jee4404 <mour03@uqo.ca>
2014-11-14 22:31:06 -0500

    relation reunion - organisateur, participants et local

--- src/business/Reunion.java
+++ src/business/Reunion.java
@@ -1,10 +1,12 @@
 package business;
 
+import com.j256.ormlite.dao.ForeignCollection;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.field.ForeignCollectionField;
 import com.j256.ormlite.table.DatabaseTable;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
@@ -15,27 +17,52 @@ import java.util.List;
 public class Reunion {
     @DatabaseField(generatedId = true, canBeNull = false)
     private Integer id;
+
     @DatabaseField(useGetSet = true, canBeNull = false)
     private Date dateReunion;
+
     @DatabaseField(useGetSet = true, canBeNull = false)
     private int nbParticipants;
+
     @DatabaseField(useGetSet = true, canBeNull = false)
     private boolean estRecurente;
-    @ForeignCollectionField()
-    private List<Employe> participants;
+
+    //@ForeignCollectionField()
+    private Collection<Employe> participants;
+
+    @DatabaseField(canBeNull = true, foreign = true, useGetSet = true)
+    private Local local;
+
+    //@DatabaseField(canBeNull = true, foreign = true, useGetSet = true)
+    private Employe organisateur;
 
     /*
         orm-lite a besoin d'un constructeur
         sans paramêtre de visibilité package au min
     * */
-    public Reunion(){}
+    public Reunion()
+    {
+        this.participants = new ArrayList<Employe>();
+    }
 
-    public Reunion(Date dateReunion, int nbParticipants, boolean estRecurente)
+    public Reunion(Date dateReunion, int nbParticipants, boolean estRecurente, Employe organisateur)
     {
         this.dateReunion = dateReunion;
         this.nbParticipants = nbParticipants;
         this.estRecurente = estRecurente;
         this.participants = new ArrayList<Employe>();
+        this.local = null;
+        this.organisateur = organisateur;
+    }
+
+    public Reunion(Date dateReunion, int nbParticipants, boolean estRecurente, Local local, Employe organisateur)
+    {
+        this.dateReunion = dateReunion;
+        this.nbParticipants = nbParticipants;
+        this.estRecurente = estRecurente;
+        this.participants = new ArrayList<Employe>();
+        this.local = local;
+        this.organisateur = organisateur;
     }
 
     public Integer getId()
@@ -48,6 +75,15 @@ public class Reunion {
         return this.nbParticipants;
     }
 
+    /*
+        utiliser estRecurente()
+        cette fonction est utilse a l'orm seulement
+     */
+    public boolean getEstRecurente()
+    {
+        return this.estRecurente;
+    }
+
     public boolean estRecurente()
     {
         return this.estRecurente;
@@ -58,9 +94,19 @@ public class Reunion {
         return this.dateReunion;
     }
 
-    public List<Employe> getParticipants()
+    public Collection<Employe> getParticipants()
     {
         return this.participants;
+    }
+
+    public Local getLocal()
+    {
+        return this.local;
+    }
+
+    public Employe getOrganisateur()
+    {
+        return this.organisateur;
     }
 
     public void setDateReunion(Date dateReunion)
@@ -88,4 +134,18 @@ public class Reunion {
         this.participants.remove(employe);
     }
 
+    public void setLocal(Local local)
+    {
+        this.local = local;
+    }
+
+    public void setParticipants(List<Employe> participants)
+    {
+        this.participants = participants;
+    }
+
+    public void setOrganisateur(Employe organisateur)
+    {
+        this.organisateur = organisateur;
+    }
 }
