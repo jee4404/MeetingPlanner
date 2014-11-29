b2cdb285df24c38c23065bb3d902b5f8a360d52b
Jee4404 <mour03@uqo.ca>
2014-11-29 14:03:39 -0500

    fonction afficher fenetre invitation participants dans controleur participant

--- src/controleurs/ControleurParticipant.java
+++ src/controleurs/ControleurParticipant.java
@@ -5,36 +5,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 import business.Employe;
+import business.ListeParticipations;
+import business.Reunion;
 import dbmanager.EmployeDBManager;
+import dbmanager.ParticipationDBManager;
+import view.frames.FenetreParticipants;
 
 public class ControleurParticipant {
+    private static ControleurParticipant controleurParticipant = new ControleurParticipant();
+	   
+    /* A private Constructor prevents any other
+    * class from instantiating.
+    */
+    private ControleurParticipant(){ }
 
-	   private static ControleurParticipant controleurParticipant = new ControleurParticipant( );
-	   
-	   /* A private Constructor prevents any other 
-	    * class from instantiating.
-	    */
-	   private ControleurParticipant(){ }
-	   
-	   /* Static 'instance' method */
-	   public static ControleurParticipant getInstance( ) {
-	      return controleurParticipant;
-	   }
-	   /* Other methods protected by singleton-ness */
-	   protected static void demoMethod( ) {
-	      System.out.println("demoMethod for ControleurParticipant"); 
-	   }
-	   
-	   /* Obtenir les employés de la base de donnée.*/
-	   public List<Employe> getListEmploye(){
-		EmployeDBManager instanceDBEmploye = EmployeDBManager.getInstance();
-		List<Employe> lstEmploye = new ArrayList<Employe>();
-		try {
-			lstEmploye = instanceDBEmploye.trouverTousEmployes();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return lstEmploye;
-	}
+    /* Static 'instance' method */
+    public static ControleurParticipant getInstance( )
+    {
+        return controleurParticipant;
+    }
+
+    public void afficherInviterParticipants(Reunion reunion)
+    {
+        try {
+            ListeParticipations listeParticipations = new ListeParticipations(reunion);
+            listeParticipations.setParticipations(ParticipationDBManager.getInstance().trouverParticipationParReunion(reunion.getId()));
+            FenetreParticipants fenetreParticipants = new FenetreParticipants(listeParticipations);
+        }
+        catch(SQLException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void inviterParticipant(int id_employe)
+    {
+
+    }
 }
