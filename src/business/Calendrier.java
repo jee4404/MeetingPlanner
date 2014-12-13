c6b5893a9f8f21b20dae724609a0f07d2b18f2c6
Mariedez <desm137@uqo.ca>
2014-12-13 15:45:53 -0500

    Nouvelle classe Calendrier

---
+++ src/business/Calendrier.java
@@ -0,0 +1,42 @@
+package business;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+public class Calendrier {
+	private List<PlageHoraire> lstPlageHoraire;
+	
+	public Calendrier(){
+		this.lstPlageHoraire = new ArrayList();
+	}
+	
+	public void addPlageHoraire(PlageHoraire plageHoraire){
+		this.lstPlageHoraire.add(plageHoraire);
+	}
+	
+	public List<PlageHoraire> getLstPlageHoraire(){
+		return this.lstPlageHoraire;
+	}
+	public boolean estDisponible(List<PlageHoraire> horaireReunion){
+		boolean disponibilite = true;
+		int i,j;
+		Date dateReunion;
+		double debutReunion;
+		i = 0;
+		while (disponibilite == true && i < horaireReunion.size()){
+			dateReunion = horaireReunion.get(i).getDate();
+			debutReunion = horaireReunion.get(i).getDebut();
+			j = 0;
+			while (disponibilite == true && j < lstPlageHoraire.size()){
+				if (dateReunion.equals(lstPlageHoraire.get(j).getDate())){
+					if (debutReunion == lstPlageHoraire.get(j).getDebut()){
+						disponibilite = false;
+					}
+				}
+				j++;
+			}
+			i++;
+		}
+		return disponibilite;
+	}
+}
